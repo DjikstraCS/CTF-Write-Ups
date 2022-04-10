f6ed1aaaade5550fd5ca6148ea450d7bef936389
DjikstraCS <34625606+DjikstraCS@users.noreply.github.com>
2022-04-10 18:22:00 +0200

    Upload

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # CTF Write-Ups
 A collection of write-ups for CTF challenges I've solved.
 
-The Python script is giving Obsidian Markdown files GitHub compatibility by fixing image paths.
+---
+## Python script:
+The Python script is recursively converting all .md files in Obsidian to GitHub compatible markdown.
+
+Does this by adding the full file path of pasted images.
+
+1.  Put it anywhere in your Obsidian vault. 
+	-  It will convert all files in that directory, plus all of it's subdirectories.
+2.  Edit the 'attachments' folder path to your local path if needed.
+3.  Execute directly from Obsidian and watch the magic happen.
 
 ---
 ## CTF Quality Tools:
--- mdConvert.py
+++ mdConvert.py
@@ -10,9 +10,9 @@ for path, dirs, files in os.walk('../'):
                 for idx, i in enumerate(lines): 
                     if i.find('![](P') == 0:
                         file_edit = True
-                        line_edit_list = i.split('(')
-                        line_edit_list.insert(1,'(./attachments/') #Edit path?
-                        lines[idx] = ''.join(line_edit_list)
+                        line = i.split('(')
+                        line.insert(1,'(./attachments/') #Edit path?
+                        lines[idx] = ''.join(line)
             if file_edit:          
                 with open(os.path.join(path, filename), 'w') as f: 
                     f.writelines(lines)
