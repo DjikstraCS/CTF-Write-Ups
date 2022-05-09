99b9d92646f02fddeb0a4f9520829cae09257ec4
DjikstraCS <34625606+DjikstraCS@users.noreply.github.com>
2022-05-09 04:15:56 +0200

    Update

--- mdConvert.py
+++ mdConvert.py
@@ -3,9 +3,9 @@
 
 import os
 
-print('\n Welcome to...\n\n  8888b   d8888 8888888b\n  88888b d88888 888  Y88b\n  888Y88888P888 888   888\n  888 Y888P 888 888   888\n  888 _Y8P_ 888 888 .d88P\n  888 ____/_888 888888P\' _____  _____/ /\n   / /   / __ \/ __ \ | / / _ \/ ___/ __/\n  / /___/ /_/ / / / / |/ /  __/ /  / /\n  \____/\____/_/ /_/|___/\___/_/   \__/')
+print('\nWelcome to...\n\n 8888b   d8888 8888888b\n 88888b d88888 888  Y88b\n 888Y88888P888 888   888\n 888 Y888P 888 888   888\n 888 _Y8P_ 888 888 .d88P\n 888 ____/_888 888888P\' _____  _____/ /\n  / /   / __ \/ __ \ | / / _ \/ ___/ __/\n / /___/ /_/ / / / / |/ /  __/ /  / /\n \____/\____/_/ /_/|___/\___/_/   \__/')
 
-if input('\n\n Start conversion? (Y/n) ').lower().strip() != 'y':
+if input('\n\nStart conversion? (Y/n) ').lower().strip() != 'y':
     exit(0)
 
 conversions = 0
@@ -26,15 +26,15 @@ for path, dirs, files in os.walk('.'):
             if file_edit:          
                 with open(os.path.join(path, filename), 'w', encoding='utf8') as f: 
                     f.writelines(lines)
-                print('\n File converted: ' + path.strip('.')[1:] + '\\' + filename)
+                print('\nFile converted: ' + path.strip('.')[1:] + '\\' + filename)
                 conversions = conversions + 1
 
 if conversions == 1:
-    print('\n 1 file converted.')
+    print('\n1 file converted.')
 elif conversions > 1:
-    print('\n ' + str(conversions) + ' files converted.')
+    print('\n' + str(conversions) + ' files converted.')
 else:
-    print('\n No files were converted.')
+    print('\nNo files were converted.')
 
-if input(' Press Enter to exit...'):
+if input('Press Enter to exit...'):
     exit(0)
